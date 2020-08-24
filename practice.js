2027e4705a306ac321ef4dfd6ccfb4becf4b0bd2
BAE-EUNGKYUNG <69333704+BAE-EUNGKYUNG@users.noreply.github.com>
2020-08-24 20:02:38 +0900

    Add files via upload

--- practice.js
+++ practice.js
@@ -33,16 +33,12 @@
 	function setLayout() {
 		// 각 스크롤 섹션의 높이 세팅
 		for (let i = 0; i < sceneInfo.length; i++) {
-			if (sceneInfo[i].type === 'sticky') {
-				sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-			} else if (sceneInfo[i].type === 'normal')  {
-                sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight;
-			}
+			ceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
 		}
 
 
-
+        setLayout();
 
 
 
